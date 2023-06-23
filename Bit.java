b2966989b84b21317bfec494531ecb4312485280
bharat7021 <bharat9819075142@gmail.com>
2023-06-23 16:50:57 +0530

    two

--- Bit.java
+++ Bit.java
@@ -5,11 +5,9 @@ public class Bit {
         int x = 0;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        char[] a = new char[3];
         while(n>0){
             String i = sc.next();
-            a = i.toCharArray();
-            if(a[1]=='+'){
+            if(i.charAt(1)=='+'){
                 x++;
             }
             else{
