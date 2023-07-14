15fb1a119c16ee39a633f31afbf7ec0f8e1c797f
bharat7021 <bharat9819075142@gmail.com>
2023-07-14 16:59:46 +0530

    sample

---
+++ NoOfDigits.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class NoOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(digit(n));
+        sc.close();
+    }
+    public static int digit(int x){
+        if(x == 0) return 0;
+        return digit(x/10)+1;
+    }
+}
