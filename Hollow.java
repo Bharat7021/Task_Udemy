ac68ac869863b2a492dd229577fff4727f98c2e8
bharat7021 <bharat9819075142@gmail.com>
2023-08-20 12:16:32 +0530

    Aug 20

--- Hollow.java
+++ Hollow.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Hollow {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int q = sc.nextInt();
         int m = sc.nextInt();
         char[] c = new char[9];
         System.out.println(Math.sqrt(Math.pow(2, 31)));
         System.out.println(c[0]);
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= q; i++) {
             for (int j = 1; j <= m; j++) {
-                if (i == 1 || j == 1 || i == n || j == m) {
+                if (i == 1 || j == 1 || i == q || j == m) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
