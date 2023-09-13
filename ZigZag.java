93dbc1fb3d269634f76249eb8211ed90bca9f6ca
bharat7021 <bharat9819075142@gmail.com>
2023-09-13 16:54:22 +0530

    sample

---
+++ ZigZag.java
@@ -0,0 +1,12 @@
+/**
+ * ZigZag
+ */
+public class ZigZag {
+
+    public static void main(String[] args) {
+        StringBuilder[] sb = new StringBuilder[3];
+        String a = "Bharat";
+        sb[0].append(a.charAt(0));
+        System.out.println(sb.toString());
+    }
+}
