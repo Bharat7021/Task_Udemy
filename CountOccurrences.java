634d844f0377cd66abd7fa9030b082c610779de6
bharat7021 <bharat9819075142@gmail.com>
2023-09-13 16:56:27 +0530

    sample

---
+++ CountOccurrences.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class CountOccurrences {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,6,6,6,6,8,10,11};
+        System.out.println(count(arr, 0, 6));
+    }
+    public static int count(int[] a, int i, int k){
+        if(i == a.length) return 0;
+        if(a[i] == k) return 1 + count(a, i + 1, k);
+        return count(a, i + 1, k);
+    }
+}
