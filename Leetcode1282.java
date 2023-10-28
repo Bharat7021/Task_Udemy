852e1e7a124969b88ebad3278fb1f675a7fc58f9
bharat7021 <bharat9819075142@gmail.com>
2023-10-28 11:36:00 +0530

    Oct 28

--- Leetcode1282.java
+++ Leetcode1282.java
@@ -1,48 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-public class Leetcode1282 {
-    class Solution {
-    public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> arr[] = new ArrayList[groupSizes.length + 1];
-        boolean vis[] = new boolean[groupSizes.length];
+// public class Leetcode1282 {
+//     class Solution {
+//     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int n = groupSizes.length + 1;
+//         List<Integer> arr[] = new ArrayList[n];
+//         boolean vis[] = new boolean[groupSizes.length];
 
-        for(int i=0 ; i< arr.length ; i++){
-            arr[i] = new ArrayList<>();
-        }
-        for(int i =0 ; i< groupSizes.length ; i++){
-            if(!vis[i]){
-            if(arr[groupSizes[i]].size()!=groupSizes[i]){
-                arr[groupSizes[i]].add(i);
-                vis[i]= true;
-            }else{
-                List<Integer> temp = new ArrayList<>();
+//         for(int i=0 ; i< arr.length ; i++){
+//             arr[i] = new ArrayList<>();
+//         }
+//         for(int i =0 ; i< groupSizes.length ; i++){
+//             if(!vis[i]){
+//             if(arr[groupSizes[i]].size()!=groupSizes[i]){
+//                 arr[groupSizes[i]].add(i);
+//                 vis[i]= true;
+//             }else{
+//                 List<Integer> temp = new ArrayList<>();
                 
-                for(int j = i ; j<groupSizes.length ; j++ ){
-                    if(groupSizes[j] == groupSizes[i]  ){
-                        temp.add(j);
-                        vis[j] = true;
-                        if(temp.size()== groupSizes[i]){
-                            break;
-                        }
-                    }
-                }
+//                 for(int j = i ; j<groupSizes.length ; j++ ){
+//                     if(groupSizes[j] == groupSizes[i]  ){
+//                         temp.add(j);
+//                         vis[j] = true;
+//                         if(temp.size()== groupSizes[i]){
+//                             break;
+//                         }
+//                     }
+//                 }
             
-                ans.add(temp);
-            }
+//                 ans.add(temp);
+//             }
 
-            }
-        }
+//             }
+//         }
 
-        for(int i=0 ; i< arr.length ; i++){
-            if(arr[i].size()!=0){
-                ans.add(arr[i]);
-            }
-        }
-        return ans;
+//         for(int i=0 ; i< arr.length ; i++){
+//             if(arr[i].size()!=0){
+//                 ans.add(arr[i]);
+//             }
+//         }
+//         return ans;
 
         
-    }
-}
-}
+//     }
+// }
+// }
---
+++ Leetcode771.java
@@ -0,0 +1,16 @@
+public class Leetcode771 {
+    class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        char[] je = jewels.toCharArray();
+        int res = 0;
+        for(int i = 0; i < stones.length(); i++){
+            for(int j = 0; j < je.length; j++){
+                if(je[j] == stones.charAt(i)){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
+}
