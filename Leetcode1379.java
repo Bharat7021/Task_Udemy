3ac14cfa0ff055dbf75a73efcf6210404cd6ed47
bharat7021 <bharat9819075142@gmail.com>
2023-10-29 22:03:01 +0530

    Oct 29

---
+++ Leetcode1379.java
@@ -0,0 +1,28 @@
+public class Leetcode1379 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+            if (original == null) {
+                return null;
+            }
+            if (original == target) {
+                return cloned;
+            }
+            TreeNode left = getTargetCopy(original.left, cloned.left, target);
+            if (left != null) {
+                return left;
+            } else {
+                return getTargetCopy(original.right, cloned.right, target);
+            }
+        }
+    }
+}
