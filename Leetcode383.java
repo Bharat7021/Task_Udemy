2f3df96723eee6e6b2f89c673d38eb19331778b2
bharat7021 <bharat9819075142@gmail.com>
2023-10-30 12:38:53 +0530

    Oct 30

---
+++ Leetcode383.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode383 {
+    class Solution {
+        public boolean canConstruct(String ransomNote, String magazine) {
+            Map<Character, Integer> m = new HashMap<>();
+            for (int i = 0; i < magazine.length(); i++) {
+                if (m.containsKey(magazine.charAt(i))) {
+                    m.put(magazine.charAt(i), m.get(magazine.charAt(i)) + 1);
+                } else {
+                    m.put(magazine.charAt(i), 1);
+                }
+            }
+            for (int i = 0; i < ransomNote.length(); i++) {
+                if (m.containsKey(ransomNote.charAt(i))) {
+                    if (m.get(ransomNote.charAt(i)) < 1) {
+                        return false;
+                    } else {
+                        m.put(ransomNote.charAt(i), m.get(ransomNote.charAt(i)) - 1);
+                    }
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
